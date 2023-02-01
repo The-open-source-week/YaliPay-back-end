f88611bd83cbaaf26ef71660430766d1fd45f43b
Lázaro Manuel Cunga <52803570+manuelcunga@users.noreply.github.com>
2023-02-01 10:08:16 +0000

    refactor: :recycle: refatorando o app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { AccountsModule } from './modules/accounts/accounts.module';
 
 @Module({
-  imports: [PrismaModule], 
+  imports: [PrismaModule, AccountsModule],
+  controllers: [], 
  
 })
 export class AppModule {}
