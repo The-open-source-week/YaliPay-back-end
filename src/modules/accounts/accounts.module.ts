fac54516d95832ce0573bfa4ba6016d9dceabce3
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-02 21:06:50 +0100

    refactor: :recycle: refatorando accounts module

--- src/modules/accounts/accounts.module.ts
+++ src/modules/accounts/accounts.module.ts
@@ -5,20 +5,32 @@ import { CreateUserController } from './controllers/create/createUser.controller
 import { CreateUserRepository } from './repositories/CreateUserRepository';
 import { PrismaCreateUserRepository } from './repositories/implementations/PrismaCreateUserRepository';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { LoginService } from './services/login/login.service';
+import { JwtStrategy } from './services/jwt-strategy/jwt.strategy.service';
+import { LocalStrategy } from './services/jwt-strategy/local-jwt.strategy.service';
+import { LoginController } from './controllers/login/login.controller';
 
 @Module({
-  controllers: [CreateUserController],
+  controllers: [CreateUserController, LoginController],
   providers: [
     CreateUserService,
+    LoginService,
+    JwtStrategy,
+    LocalStrategy,
 
     {
       provide: CreateUserRepository,
-      useClass: PrismaCreateUserRepository
-    }
-  
+      useClass: PrismaCreateUserRepository,
+    },
   ],
   imports: [
-    PrismaModule
-  ]
+    PrismaModule,
+    JwtModule.register({
+      signOptions: {
+        expiresIn: '30d',
+      },
+    }),
+  ],
 })
 export class AccountsModule {}
