0f9c490947f9bd3a6b579b5a1533dfb17d0b7f22
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-02 21:10:51 +0100

    refactor: :recycle: refatorando createUser repository

--- src/modules/accounts/repositories/CreateUserRepository.ts
+++ src/modules/accounts/repositories/CreateUserRepository.ts
@@ -1,5 +1,6 @@
+import { Account } from '@prisma/client';
 import { CreateUserDTO } from '../dtos/create-user.dto';
-export abstract class CreateUserRepository{
-  abstract create(userDto: CreateUserDTO): Promise<void>
+export abstract class CreateUserRepository {
+  abstract create(userDto: CreateUserDTO): Promise<void>;
+  abstract findByEmail(email: string): Promise<Account>;
 }
-
