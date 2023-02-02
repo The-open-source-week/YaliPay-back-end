383603aafc01583b8b7c23723a958e6df6a74341
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-02 21:11:11 +0100

    refactor: :recycle: refatorando implementação de repositorio do usuário

--- src/modules/accounts/repositories/implementations/PrismaCreateUserRepository.ts
+++ src/modules/accounts/repositories/implementations/PrismaCreateUserRepository.ts
@@ -1,24 +1,33 @@
-import { PrismaService } from "src/modules/prisma/prisma.service";
-import { CreateUserDTO } from "../../dtos/create-user.dto";
-import { CreateUserRepository } from "../CreateUserRepository";
+import { PrismaService } from 'src/modules/prisma/prisma.service';
+import { CreateUserDTO } from '../../dtos/create-user.dto';
+import { CreateUserRepository } from '../CreateUserRepository';
 import { Injectable } from '@nestjs/common';
-import { randomUUID } from "crypto";
-
+import { Account } from '@prisma/client';
 
 @Injectable()
-export class PrismaCreateUserRepository implements CreateUserRepository{
-    constructor(private readonly Prisma: PrismaService){}
-  
- async create(userDto: CreateUserDTO): Promise<void> {
-     await this.Prisma.account.create({
+export class PrismaCreateUserRepository implements CreateUserRepository {
+  constructor(private readonly Prisma: PrismaService) {}
+
+  async create(userDto: CreateUserDTO): Promise<void> {
+    await this.Prisma.account.create({
       data: {
         username: userDto.username,
-        email:   userDto.email,
+        email: userDto.email,
         phoneNumber: userDto.phoneNumber,
-        password:  userDto.password,
-        code:     userDto.code
-      }
-    })
+        password: userDto.password,
+        code: userDto.code,
+        accountNumber: userDto.accountNumber,
+      },
+    });
   }
-  
+
+  async findByEmail(email: string): Promise<Account> {
+    const user = await this.Prisma.account.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    return user;
+  }
 }
