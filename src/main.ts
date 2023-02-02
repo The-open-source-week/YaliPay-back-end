1fb79ec001f21f0f39830721933144b6f3bf510f
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-02 21:06:41 +0100

    refactor: :recycle: refatorando main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { config } from './doc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api/v1")
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
