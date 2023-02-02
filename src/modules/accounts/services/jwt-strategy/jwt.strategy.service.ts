dbedeb3320956cafe90b82b2b62ad6e12932393f
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-02 21:11:49 +0100

    feat: :sparkles: adicionando  jwt service estretegia

---
+++ src/modules/accounts/services/jwt-strategy/jwt.strategy.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_KEY,
+    });
+  }
+
+  async validate(payload: any) {
+    return { sub: payload.id, email: payload.email };
+  }
+}
