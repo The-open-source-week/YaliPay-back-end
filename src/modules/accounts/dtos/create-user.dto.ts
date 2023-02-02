039814e66b993e89710df53e9bc3312db89cb572
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-02 21:10:26 +0100

    refactor: :recycle: refatorando dto create user

--- src/modules/accounts/dtos/create-user.dto.ts
+++ src/modules/accounts/dtos/create-user.dto.ts
@@ -1,7 +1,35 @@
-export class CreateUserDTO{
-  username   : string
-  email      : string
-  password   : string
-  phoneNumber: string
-  code       : number 
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  phoneNumber: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  code: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  accountNumber: string;
 }
