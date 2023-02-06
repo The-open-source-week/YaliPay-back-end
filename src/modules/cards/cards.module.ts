a1cf2fef3bfd68eb452cac7986906be7c6a2dffa
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-06 20:03:43 +0100

    refactor: :recycle: refatorando

--- src/modules/cards/cards.module.ts
+++ src/modules/cards/cards.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ICardsRepository } from './repositories/cardsRepository';
 import { PrismaCardsRepository } from './repositories/implementations/prismaCardsRepository';
 import { PrismaModule } from '../prisma/prisma.module';
+import { CreateCardService } from './services/create/createCard.service';
+import { CreateCardController } from './controllers/create-card.controller';
 
 @Module({
-  controllers: [],
+  controllers: [CreateCardController],
   providers: [
+    CreateCardService,
+
     {
       provide: ICardsRepository,
       useClass: PrismaCardsRepository,
