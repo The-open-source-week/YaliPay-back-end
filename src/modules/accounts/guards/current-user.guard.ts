00cddae42f2100e915909695f889250fcd0d52e8
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-06 13:56:26 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/guards/current-user.guard.ts
+++ src/modules/accounts/guards/current-user.guard.ts
@@ -1,8 +1,16 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 
 export const Me = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
+  (data: unknown, ctx: ExecutionContext): Request['user'] => {
+    const request = ctx.switchToHttp().getRequest<Request>();
     return request.user;
   },
 );
+
+// export const Me = createParamDecorator(
+//   (data: unknown, ctx: ExecutionContext) => {
+//     const request = ctx.switchToHttp().getRequest();
+//     return request.user;
+//   },
+// );
