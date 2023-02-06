7a201b7cd05150895b6c25963cf10df9b59d6251
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-06 20:03:40 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/services/create/createUser.service.ts
+++ src/modules/accounts/services/create/createUser.service.ts
@@ -4,15 +4,24 @@ import { CreateUserDTO } from '../../dtos/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { GenerateAccountNumber } from '../../../../utils/GenerateAccountsNumber';
 import { GenerateConfirmationCode } from '../../../../utils/GenerateConfirmationCode';
+import { ICardsRepository } from 'src/modules/cards/repositories/cardsRepository';
+import { CreateCardDTO } from 'src/modules/cards/dtos/create-card.dto';
+import { GenerateCVV } from 'src/utils/GenerateCVV';
+import { Account } from '@prisma/client';
 
 @Injectable()
 export class CreateUserService {
-  constructor(private userRepository: CreateUserRepository) {}
+  constructor(
+    private userRepository: CreateUserRepository,
+    private cardRepository: ICardsRepository,
+  ) {}
 
-  async execute(data: CreateUserDTO): Promise<void> {
+  async execute(data: CreateUserDTO, cardDTO: CreateCardDTO): Promise<Account> {
     const passwordHash = (data.password = bcrypt.hashSync(data.password, 12));
+    const cardNumber = Math.random().toString(36).substr(2, 9);
+    const expiretDate = new Date('2025-07-12T14:30:00Z');
 
-    await this.userRepository.create({
+    const user = await this.userRepository.create({
       username: data.username,
       accountNumber: GenerateAccountNumber(),
       code: GenerateConfirmationCode(),
@@ -20,5 +29,17 @@ export class CreateUserService {
       email: data.email,
       phoneNumber: data.phoneNumber,
     });
+
+    await this.cardRepository.create({
+      amount: 0,
+      cardNumber: Number(cardNumber),
+      cardHolderName: cardDTO.cardHolderName,
+      isActive: true,
+      cvv: GenerateCVV(),
+      accountId: user.id,
+      expireAt: expiretDate,
+    });
+
+    return user;
   }
 }
