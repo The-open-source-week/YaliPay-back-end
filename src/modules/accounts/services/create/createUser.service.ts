5524d404600fc5d917ff868bb52d81cad30f0615
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-06 14:03:25 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/services/create/createUser.service.ts
+++ src/modules/accounts/services/create/createUser.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserRepository } from '../../repositories/CreateUserRepository';
 import { CreateUserDTO } from '../../dtos/create-user.dto';
 import * as bcrypt from 'bcrypt';
-import { GenerateAccountNumber } from 'src/utils/GenerateAccountsNumber';
-import { GenerateConfirmationCode } from 'src/utils/GenerateConfirmationCode';
+import { GenerateAccountNumber } from '../../../../utils/GenerateAccountsNumber';
+import { GenerateConfirmationCode } from '../../../../utils/GenerateConfirmationCode';
 
 @Injectable()
 export class CreateUserService {
