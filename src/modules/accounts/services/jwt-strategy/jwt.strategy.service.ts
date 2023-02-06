df0fd670d7b2e28775b006783764535849643ca7
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-06 13:27:42 +0100

    refactor: :recycle:  estrategia jwt

--- src/modules/accounts/services/jwt-strategy/jwt.strategy.service.ts
+++ src/modules/accounts/services/jwt-strategy/jwt.strategy.service.ts
@@ -7,6 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
       secretOrKey: process.env.JWT_KEY,
     });
   }
