b70c0cd0369d5b8acbaaf04e073ae1e451037cb3
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-06 20:02:06 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/controllers/create/createUser.controller.ts
+++ src/modules/accounts/controllers/create/createUser.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { CreateUserService } from '../../services/create/createUser.service';
 import { CreateUserDTO } from '../../dtos/create-user.dto';
+import { CreateCardDTO } from 'src/modules/cards/dtos/create-card.dto';
 
 @Controller('user')
 export class CreateUserController {
@@ -8,7 +9,7 @@ export class CreateUserController {
 
   @Post('/create')
   @HttpCode(HttpStatus.CREATED)
-  create(@Body() createUserDTO: CreateUserDTO) {
-    return this.userService.execute(createUserDTO);
+  create(@Body() createUserDTO: CreateUserDTO, cardDTO: CreateCardDTO) {
+    return this.userService.execute(createUserDTO, cardDTO);
   }
 }
