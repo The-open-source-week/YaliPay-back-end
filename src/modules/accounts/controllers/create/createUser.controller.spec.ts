b94f3665c8e98365fc5a13ff430cbb956255d86e
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-06 13:58:48 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/controllers/create/createUser.controller.spec.ts
+++ src/modules/accounts/controllers/create/createUser.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import {CreateUserController} from "./create.controller"
+import { CreateUserController } from './createUser.controller';
 
 describe('CreateController', () => {
   let controller: CreateUserController;
