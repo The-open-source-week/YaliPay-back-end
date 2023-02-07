7c0e374e2a0250e7f31cd917d0b034717286b5b5
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-07 18:38:39 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/guards/current-user.guard.ts
+++ src/modules/accounts/guards/current-user.guard.ts
@@ -1,16 +1,6 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { Request } from 'express';
 
-// @Injectable()
-// export class CurrentUserGuard implements CanActivate {
-//   canActivate(
-//     context: ExecutionContext,
-//   ): boolean | Promise<boolean> | Observable<boolean> {
-//     const request = context.switchToHttp().getRequest();
-//     return !!request.user;
-//   }
-// }
-
 export const Me = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): Request['user'] => {
     const request = ctx.switchToHttp().getRequest<Request>();
