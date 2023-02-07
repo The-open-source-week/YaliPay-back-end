3e17885066f09b09c79f2351ed1b0d49a582964d
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-07 18:39:03 +0100

    refactor: :recycle: refatorando

--- src/modules/cards/cards.module.ts
+++ src/modules/cards/cards.module.ts
@@ -3,12 +3,22 @@ import { ICardsRepository } from './repositories/cardsRepository';
 import { PrismaCardsRepository } from './repositories/implementations/prismaCardsRepository';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CreateCardService } from './services/create/createCard.service';
-import { CreateCardController } from './controllers/create-card.controller';
+import { CreateCardController } from './controllers/create/create-card.controller';
+import { ListAllCardService } from './services/listAll/ListaAllCard.service';
+import { ListAllCardController } from './controllers/listAll/findAll-card.controller';
+import { ShowCardController } from './controllers/ShowCard/show-card.controller';
+import { ShowCardService } from './services/ShowCard/ShowCard.service';
 
 @Module({
-  controllers: [CreateCardController],
+  controllers: [
+    CreateCardController,
+    ListAllCardController,
+    ShowCardController,
+  ],
   providers: [
     CreateCardService,
+    ListAllCardService,
+    ShowCardService,
 
     {
       provide: ICardsRepository,
