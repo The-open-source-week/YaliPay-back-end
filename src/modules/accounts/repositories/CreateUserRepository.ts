261290a020b3c4b69f1ddec940a91d697bb98c6c
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-07 18:38:44 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/repositories/CreateUserRepository.ts
+++ src/modules/accounts/repositories/CreateUserRepository.ts
@@ -3,4 +3,6 @@ import { CreateUserDTO } from '../dtos/create-user.dto';
 export abstract class CreateUserRepository {
   abstract create(userDto: CreateUserDTO): Promise<Account>;
   abstract findByEmail(email: string): Promise<Account>;
+  abstract findByPhone(phone: string): Promise<Account>;
+  abstract findById(id: string): Promise<Account>;
 }
