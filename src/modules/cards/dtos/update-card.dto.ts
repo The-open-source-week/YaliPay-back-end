266f536e4ea538d0786246104738310ff7c7d0d6
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-07 18:39:28 +0100

    refactor: :recycle: refatorando

--- src/modules/cards/dtos/update-card.dto.ts
+++ src/modules/cards/dtos/update-card.dto.ts
@@ -1,24 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
 
 export class UpdateCardDTO {
   @IsString()
   @ApiProperty({ required: true })
   accountId?: string;
 
-  @IsString()
-  @ApiProperty({ required: true })
-  cardHolderName?: string;
-
   @IsNumber()
   @ApiProperty({ required: true })
   cardNumber?: number;
 
   @IsNumber()
-  @ApiProperty({ required: false })
   cvv?: number;
 
-  @ApiProperty({ required: false })
+  @IsDate()
   expireAt?: Date;
 
   @IsNumber()
