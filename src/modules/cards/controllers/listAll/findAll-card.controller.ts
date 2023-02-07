045c1ac3d04370c7e72ec38557ab1d6ea5a78ade
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-07 18:39:16 +0100

    refactor: :recycle: refatorando

---
+++ src/modules/cards/controllers/listAll/findAll-card.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+import { AuthUserGuard } from '../../../accounts/guards/auth.guard';
+import { Me } from 'src/modules/accounts/guards/current-user.guard';
+import { ListAllCardService } from '../../services/listAll/ListaAllCard.service';
+
+@UseGuards(AuthUserGuard)
+@Controller('cards')
+export class ListAllCardController {
+  constructor(private listAllService: ListAllCardService) {}
+
+  @Get('/')
+  @ApiProperty()
+  @HttpCode(HttpStatus.OK)
+  create(@Body() accountID: string, @Me() user: any) {
+    accountID = user.sub.sub;
+    return this.listAllService.execute(accountID);
+  }
+}
