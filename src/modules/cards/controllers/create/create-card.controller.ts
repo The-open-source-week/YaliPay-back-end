19ea0d780c40fbf23655a765da2ae8a7b6bd0964
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-07 18:39:12 +0100

    refactor: :recycle: refatorando

---
+++ src/modules/cards/controllers/create/create-card.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateCardDTO } from '../../../../modules/cards/dtos/create-card.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { CreateCardService } from '../../services/create/createCard.service';
+import { AuthUserGuard } from '../../../../modules/accounts/guards/auth.guard';
+import { Me } from 'src/modules/accounts/guards/current-user.guard';
+
+@Controller('card')
+export class CreateCardController {
+  constructor(private cardService: CreateCardService) {}
+
+  @UseGuards(AuthUserGuard)
+  @Post('/create')
+  @ApiProperty()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() cardDTO: CreateCardDTO, @Me() user: any) {
+    cardDTO.accountId = user.sub.sub;
+    return this.cardService.execute(cardDTO);
+  }
+}
