53dcd6b73a7657400eb15f568f0ac759d3e1fc40
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-07 18:38:13 +0100

    refactor: :recycle: refatorando

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { AccountsModule } from './modules/accounts/accounts.module';
 import { CardsModule } from './modules/cards/cards.module';
+import { TransferencesModule } from './modules/transferences/transferences.module';
 
 @Module({
-  imports: [PrismaModule, AccountsModule, CardsModule],
+  imports: [PrismaModule, AccountsModule, CardsModule, TransferencesModule],
   controllers: [],
 })
 export class AppModule {}
