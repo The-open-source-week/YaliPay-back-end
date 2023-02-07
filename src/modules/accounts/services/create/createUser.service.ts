f608f57ab883051cfd81848d6446760014978c17
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-07 18:38:53 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/services/create/createUser.service.ts
+++ src/modules/accounts/services/create/createUser.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserRepository } from '../../repositories/CreateUserRepository';
 import { CreateUserDTO } from '../../dtos/create-user.dto';
 import * as bcrypt from 'bcrypt';
@@ -16,27 +16,43 @@ export class CreateUserService {
   ) {}
 
   async execute(data: CreateUserDTO): Promise<Account> {
-    const passwordHash = (data.password = bcrypt.hashSync(data.password, 12));
-    const expiretDate = new Date('2025-07-12T14:30:00Z');
+    try {
+      const userAlready = await this.userRepository.findByEmail(data.email);
 
-    const user = await this.userRepository.create({
-      username: data.username,
-      accountNumber: GenerateAccountNumber(),
-      code: GenerateConfirmationCode(),
-      password: passwordHash,
-      email: data.email,
-      phoneNumber: data.phoneNumber,
-    });
+      if (userAlready) {
+        throw new HttpException(
+          {
+            status: HttpStatus.CONFLICT,
+            error: 'Usuário já existe',
+          },
+          HttpStatus.CONFLICT,
+        );
+      }
 
-    await this.cardRepository.create({
-      amount: 0,
-      cardNumber: GenerateAccountNumber(),
-      isActive: true,
-      cvv: GenerateCVV(),
-      accountId: user.id,
-      expireAt: expiretDate,
-    });
+      const passwordHash = (data.password = bcrypt.hashSync(data.password, 12));
+      const expiretDate = new Date('2025-07-12T14:30:00Z');
 
-    return user;
+      const user = await this.userRepository.create({
+        username: data.username,
+        accountNumber: GenerateAccountNumber(),
+        code: GenerateConfirmationCode(),
+        password: passwordHash,
+        email: data.email,
+        phoneNumber: data.phoneNumber,
+      });
+
+      await this.cardRepository.create({
+        amount: 0,
+        cardNumber: GenerateAccountNumber(),
+        isActive: true,
+        cvv: GenerateCVV(),
+        accountId: user.id,
+        expireAt: expiretDate,
+      });
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
   }
 }
