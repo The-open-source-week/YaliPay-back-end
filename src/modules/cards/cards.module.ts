e92ba35aa51741f8e8a49d12e88879df1bcfe2de
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-08 17:05:39 +0100

    refactor: :recycle: refatorando

--- src/modules/cards/cards.module.ts
+++ src/modules/cards/cards.module.ts
@@ -6,19 +6,35 @@ import { CreateCardService } from './services/create/createCard.service';
 import { CreateCardController } from './controllers/create/create-card.controller';
 import { ListAllCardService } from './services/listAll/ListaAllCard.service';
 import { ListAllCardController } from './controllers/listAll/findAll-card.controller';
-import { ShowCardController } from './controllers/ShowCard/show-card.controller';
-import { ShowCardService } from './services/ShowCard/ShowCard.service';
+import { GetMainCardService } from './services/getMainCard/GetMainCard.service';
+import { GetMainCardController } from './controllers/getMainCard/GetMaincard.controller';
+import { ActiveMainCardController } from './controllers/activeMainCard/ActiveMaincard.controller';
+import { ActiveMainCardService } from './services/activateMainCard/ActiveMainCard.service';
+import { RemoveCardService } from './services/removeCard/RemoveCard.service';
+import { RemoveCardController } from './controllers/removeCard/RemoveCard.controller';
+import { ActiveCardService } from './services/ActiveCard /activeCard.service';
+import { DesactiveCardService } from './services/DesactiveCard /desactiveCard.service';
+import { DesactiveCardController } from './controllers/desactiveCard/desactiveCard.controller';
+import { ActiveCardController } from './controllers/activeCard /activeCard.controller';
 
 @Module({
   controllers: [
     CreateCardController,
     ListAllCardController,
-    ShowCardController,
+    GetMainCardController,
+    ActiveMainCardController,
+    RemoveCardController,
+    DesactiveCardController,
+    ActiveCardController,
   ],
   providers: [
     CreateCardService,
     ListAllCardService,
-    ShowCardService,
+    GetMainCardService,
+    ActiveMainCardService,
+    RemoveCardService,
+    ActiveCardService,
+    DesactiveCardService,
 
     {
       provide: ICardsRepository,
