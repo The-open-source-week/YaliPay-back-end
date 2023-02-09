0e2494857b0786e667e3ef9c5bf086571e9bb95c
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 12:20:58 +0100

    refactor: :recycle: refatorando

--- src/modules/cards/services/DesactiveCard /desactiveCard.service.spec.ts
+++ src/modules/cards/services/DesactiveCard /desactiveCard.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DesactiveCardService } from './desactiveCard.service';
+import { PrismaModule } from '../../../../modules/prisma/prisma.module';
+import { ICardsRepository } from '../../repositories/cardsRepository';
+import { PrismaCardsRepository } from '../../repositories/implementations/prismaCardsRepository';
 
 describe('DesactiveCardService', () => {
   let service: DesactiveCardService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DesactiveCardService],
+      providers: [
+        DesactiveCardService,
+        {
+          provide: ICardsRepository,
+          useClass: PrismaCardsRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<DesactiveCardService>(DesactiveCardService);
