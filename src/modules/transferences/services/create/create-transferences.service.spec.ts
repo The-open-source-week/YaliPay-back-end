a352f6f062544ba168843a82d78719f54a3be0b2
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 12:21:33 +0100

    refactor: :recycle: refatorando

--- src/modules/transferences/services/create/create-transferences.service.spec.ts
+++ src/modules/transferences/services/create/create-transferences.service.spec.ts
@@ -1,20 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateTransferencesService } from './create-transferences.service';
+import { CreateTransferenceRepository } from '../../repositories/CreateTransferenceRepositories';
+import { PrismaCreateTransferenceRepository } from '../../repositories/implementations/PrismaCreateTransferencesRepositories';
+import { ICardsRepository } from '../../../../modules/cards/repositories/cardsRepository';
+import { PrismaCardsRepository } from '../../../../modules/cards/repositories/implementations/prismaCardsRepository';
+import { CreateUserRepository } from '../../../../modules/accounts/repositories/CreateUserRepository';
+import { PrismaCreateUserRepository } from '../../../../modules/accounts/repositories/implementations/PrismaCreateUserRepository';
+import { PrismaModule } from '../../../../modules/prisma/prisma.module';
 
 describe('TransferencesService', () => {
-  let service: CreateTransferencesService;
+  let createTransferenceService: CreateTransferencesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateTransferencesService],
+      providers: [
+        CreateTransferencesService,
+        {
+          provide: CreateTransferenceRepository,
+          useClass: PrismaCreateTransferenceRepository,
+        },
+        {
+          provide: ICardsRepository,
+          useClass: PrismaCardsRepository,
+        },
+        {
+          provide: CreateUserRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
-    service = module.get<CreateTransferencesService>(
+    createTransferenceService = module.get<CreateTransferencesService>(
       CreateTransferencesService,
     );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(createTransferenceService).toBeDefined();
   });
 });
