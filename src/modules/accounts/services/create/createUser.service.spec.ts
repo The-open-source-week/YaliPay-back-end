3d09f430b582302c8c4e35e8ec8c8ae613c43e61
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 12:08:56 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/services/create/createUser.service.spec.ts
+++ src/modules/accounts/services/create/createUser.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserService } from './createUser.service';
+import { PrismaModule } from '../../../../modules/prisma/prisma.module';
+import { CreateUserRepository } from '../../repositories/CreateUserRepository';
+import { PrismaCreateUserRepository } from '../../repositories/implementations/PrismaCreateUserRepository';
+import { ICardsRepository } from '../../../../modules/cards/repositories/cardsRepository';
+import { PrismaCardsRepository } from '../../../../modules/cards/repositories/implementations/prismaCardsRepository';
 
 describe('CreateService', () => {
   let service: CreateUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateUserService],
+      providers: [
+        CreateUserService,
+        {
+          provide: CreateUserRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+        {
+          provide: ICardsRepository,
+          useClass: PrismaCardsRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<CreateUserService>(CreateUserService);
