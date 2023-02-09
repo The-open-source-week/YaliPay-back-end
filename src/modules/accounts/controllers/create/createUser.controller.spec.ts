38a21397ba5d37ea6ae773c1eaeaa708d8effbc8
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 12:08:01 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/controllers/create/createUser.controller.spec.ts
+++ src/modules/accounts/controllers/create/createUser.controller.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserController } from './createUser.controller';
+import { CreateUserRepository } from '../../repositories/CreateUserRepository';
+import { PrismaCreateUserRepository } from '../../repositories/implementations/PrismaCreateUserRepository';
+import { ICardsRepository } from '../../../../modules/cards/repositories/cardsRepository';
+import { PrismaCardsRepository } from '../../../../modules/cards/repositories/implementations/prismaCardsRepository';
+import { CreateUserService } from '../../services/create/createUser.service';
+import { PrismaModule } from '../../../../modules/prisma/prisma.module';
 
 describe('CreateController', () => {
   let controller: CreateUserController;
@@ -7,6 +13,18 @@ describe('CreateController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CreateUserController],
+      providers: [
+        CreateUserService,
+        {
+          provide: CreateUserRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+        {
+          provide: ICardsRepository,
+          useClass: PrismaCardsRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     controller = module.get<CreateUserController>(CreateUserController);
