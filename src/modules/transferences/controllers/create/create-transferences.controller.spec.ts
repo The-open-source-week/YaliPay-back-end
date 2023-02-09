7ec3d3f5bcb9781ef089fac452a792c78edf268c
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 12:21:24 +0100

    refactor: :recycle: refatorando

--- src/modules/transferences/controllers/create/create-transferences.controller.spec.ts
+++ src/modules/transferences/controllers/create/create-transferences.controller.spec.ts
@@ -1,6 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateTransferencesController } from './create-transferences.controller';
 import { CreateTransferencesService } from '../../services/create/create-transferences.service';
+import { CreateTransferenceRepository } from '../../repositories/CreateTransferenceRepositories';
+import { PrismaCreateTransferenceRepository } from '../../repositories/implementations/PrismaCreateTransferencesRepositories';
+import { ICardsRepository } from '../../../../modules/cards/repositories/cardsRepository';
+import { PrismaCardsRepository } from '../../../../modules/cards/repositories/implementations/prismaCardsRepository';
+import { CreateUserRepository } from '../../../../modules/accounts/repositories/CreateUserRepository';
+import { PrismaCreateUserRepository } from '../../../../modules/accounts/repositories/implementations/PrismaCreateUserRepository';
+import { PrismaModule } from '../../../../modules/prisma/prisma.module';
 
 describe('TransferencesController', () => {
   let controller: CreateTransferencesController;
@@ -8,7 +15,22 @@ describe('TransferencesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CreateTransferencesController],
-      providers: [CreateTransferencesService],
+      providers: [
+        CreateTransferencesService,
+        {
+          provide: CreateTransferenceRepository,
+          useClass: PrismaCreateTransferenceRepository,
+        },
+        {
+          provide: ICardsRepository,
+          useClass: PrismaCardsRepository,
+        },
+        {
+          provide: CreateUserRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     controller = module.get<CreateTransferencesController>(
