92bc535f9259b8404bcd6bd7f5c82eae70674d02
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 12:20:43 +0100

    refactor: :recycle: refatorando

--- src/modules/cards/services/activateMainCard/ActiveMainCard.service.spec.ts
+++ src/modules/cards/services/activateMainCard/ActiveMainCard.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ActiveMainCardService } from './ActiveMainCard.service';
+import { PrismaModule } from '../../../../modules/prisma/prisma.module';
+import { ICardsRepository } from '../../repositories/cardsRepository';
+import { PrismaCardsRepository } from '../../repositories/implementations/prismaCardsRepository';
 
 describe('ActiveMainCardService', () => {
   let service: ActiveMainCardService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ActiveMainCardService],
+      providers: [
+        ActiveMainCardService,
+        {
+          provide: ICardsRepository,
+          useClass: PrismaCardsRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<ActiveMainCardService>(ActiveMainCardService);
