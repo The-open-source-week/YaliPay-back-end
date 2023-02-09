3a1d7d7fcc173f138e764bc77f45b12aab6a738a
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 12:09:04 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/services/login/login.service.spec.ts
+++ src/modules/accounts/services/login/login.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoginService } from './login.service';
+import { LocalStrategy } from '../jwt-strategy/local-jwt.strategy.service';
+import { JwtStrategy } from '../jwt-strategy/jwt.strategy.service';
+import { PrismaModule } from '../../../../modules/prisma/prisma.module';
+import { CreateUserRepository } from '../../repositories/CreateUserRepository';
+import { PrismaCreateUserRepository } from '../../repositories/implementations/PrismaCreateUserRepository';
+import { ICardsRepository } from '../../../../modules/cards/repositories/cardsRepository';
+import { PrismaCardsRepository } from '../../../../modules/cards/repositories/implementations/prismaCardsRepository';
+import { JwtService } from '@nestjs/jwt';
 
 describe('LoginService', () => {
   let service: LoginService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LoginService],
+      providers: [
+        LoginService,
+        LocalStrategy,
+        JwtStrategy,
+        JwtService,
+        {
+          provide: CreateUserRepository,
+          useClass: PrismaCreateUserRepository,
+        },
+        {
+          provide: ICardsRepository,
+          useClass: PrismaCardsRepository,
+        },
+      ],
+      imports: [PrismaModule],
     }).compile();
 
     service = module.get<LoginService>(LoginService);
