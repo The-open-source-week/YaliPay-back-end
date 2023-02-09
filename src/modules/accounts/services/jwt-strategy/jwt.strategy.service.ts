e6ee1c9acf2dc8daec26615657d26acc7a70a0a7
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-02-09 12:42:39 +0100

    refactor: :recycle: refatorando

--- src/modules/accounts/services/jwt-strategy/jwt.strategy.service.ts
+++ src/modules/accounts/services/jwt-strategy/jwt.strategy.service.ts
@@ -2,11 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
-interface JwtPayload {
-  id: string;
-  email: string;
-}
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
